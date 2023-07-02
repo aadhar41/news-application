3c3f453aa3a9e3af458117e2e788214f074f9382
Aadhar gaur <aadhar41@gmail.com>
2023-07-02 09:26:52 +0530

    1. Creating React App

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
